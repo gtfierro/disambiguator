b2c41090bc8449e00e53533b6e86ac2e1fdcf5a7
Gabe Fierro <gtfierro225@gmail.com>
2013-11-21 14:44:51 -0800

    go fmt

--- database.go
+++ database.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "fmt"
-    rethink "github.com/christopherhesse/rethinkgo"
-    "sync/atomic"
+	"fmt"
+	rethink "github.com/christopherhesse/rethinkgo"
+	"sync/atomic"
 )
 
 var collectionCount = int32(0)
@@ -28,93 +28,91 @@ var collectionCount = int32(0)
 */
 
 func runDisambiguations() {
-    var response []interface{}
-    for cc := 0; int32(cc) < collectionCount; cc += 1 {
-        go func() {
-            session, err := rethink.Connect("localhost:28015", "disambiguate")
-            if err != nil {
-                fmt.Println("err connection:", err)
-                return
-            }
-            frequencies := make(map[string]int)
-            rethink.Table("records").Filter(rethink.Map{"collectionID": cc}).Run(session).All(&response)
-            for _, i := range response {
-                r := convertInterfaceToRecord(i)
-                frequencies[r.clean] += 1
-            }
-            actual := ""
-            max := 0
-            for str, count := range frequencies {
-                if count > max {
-                    max = count
-                    actual = str
-                }
-            }
-            var writeresp rethink.WriteResponse
-            rethink.Table("records").Filter(rethink.Map{"collectionID": cc}).Update(rethink.Map{"actual":actual}).Run(session).All(&writeresp)
-        }()
-    }
+	var response []interface{}
+	for cc := 0; int32(cc) < collectionCount; cc += 1 {
+		go func() {
+			session, err := rethink.Connect("localhost:28015", "disambiguate")
+			if err != nil {
+				fmt.Println("err connection:", err)
+				return
+			}
+			frequencies := make(map[string]int)
+			rethink.Table("records").Filter(rethink.Map{"collectionID": cc}).Run(session).All(&response)
+			for _, i := range response {
+				r := convertInterfaceToRecord(i)
+				frequencies[r.clean] += 1
+			}
+			actual := ""
+			max := 0
+			for str, count := range frequencies {
+				if count > max {
+					max = count
+					actual = str
+				}
+			}
+			var writeresp rethink.WriteResponse
+			rethink.Table("records").Filter(rethink.Map{"collectionID": cc}).Update(rethink.Map{"actual": actual}).Run(session).All(&writeresp)
+		}()
+	}
 }
 
 func printOutput() {
-    session, _ := rethink.Connect("localhost:28015", "disambiguate")
-    response := rethink.Table("records").Run(session)
-    i := 0
-    for response.Next() {
-        i += 1
-        var r interface{}
-        if err := response.Scan(&r); err != nil {
-            fmt.Println("err:", err)
-            break
-        }
-        rec := convertInterfaceToRecord(r)
-        fmt.Print(rec.raw)
-        fmt.Print("\t")
-        fmt.Print(rec.actual)
-        fmt.Print("\t")
-        fmt.Println(rec.collectionID)
+	session, _ := rethink.Connect("localhost:28015", "disambiguate")
+	response := rethink.Table("records").Run(session)
+	i := 0
+	for response.Next() {
+		i += 1
+		var r interface{}
+		if err := response.Scan(&r); err != nil {
+			fmt.Println("err:", err)
+			break
+		}
+		rec := convertInterfaceToRecord(r)
+		fmt.Print(rec.raw)
+		fmt.Print("\t")
+		fmt.Print(rec.actual)
+		fmt.Print("\t")
+		fmt.Println(rec.collectionID)
 
-    }
+	}
 }
 
-
-
 func findMatch(rec *Record) {
-    session, err := rethink.Connect("localhost:28015", "disambiguate")
-    if err != nil {
-        fmt.Println("err connection:", err)
-        return
-    }
-    var response []interface{}
-    for cc := 0; int32(cc) < collectionCount; cc += 1 {
-        rethink.Table("records").Filter(rethink.Map{"collectionID": cc}).Run(session).All(&response)
-        for _, i := range response {
-            r := convertInterfaceToRecord(i)
-            if rec.getRawSimilarity(r) >= .9 {
-                addRecordToCollection(session, rec, int32(cc))
-                return
-            }
-        }
-    }
-    // if no similarities found
-    addRecord(session, rec)
+	session, err := rethink.Connect("localhost:28015", "disambiguate")
+	if err != nil {
+		fmt.Println("err connection:", err)
+		return
+	}
+	var response []interface{}
+	for cc := 0; int32(cc) < collectionCount; cc += 1 {
+		rethink.Table("records").Filter(rethink.Map{"collectionID": cc}).Run(session).All(&response)
+		for _, i := range response {
+			r := convertInterfaceToRecord(i)
+			if rec.getRawSimilarity(r) >= .9 {
+				addRecordToCollection(session, rec, int32(cc))
+				return
+			}
+		}
+	}
+	// if no similarities found
+	addRecord(session, rec)
 }
 
 func addRecord(session *rethink.Session, rec *Record) {
-    var response rethink.WriteResponse
-    atomic.AddInt32(&collectionCount, 1)
-    insert := rethink.Map{"raw": rec.raw, "clean": rec.clean, "collectionID": collectionCount}
-    err := rethink.Table("records").Insert(insert).Run(session).One(&response)
-    if err != nil {
-        fmt.Println(err)
-    }
+	var response rethink.WriteResponse
+	atomic.AddInt32(&collectionCount, 1)
+	insert := rethink.Map{"raw": rec.raw, "clean": rec.clean, "collectionID": collectionCount}
+	err := rethink.Table("records").Insert(insert).Run(session).One(&response)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func addRecordToCollection(session *rethink.Session, rec *Record, collectionID int32) {
-    var response rethink.WriteResponse
-    insert := rethink.Map{"raw": rec.raw, "clean": rec.clean, "collectionID": collectionID}
-    err := rethink.Table("records").Insert(insert).Run(session).One(&response)
-    if err != nil {
-        fmt.Println(err)
-    }
+	var response rethink.WriteResponse
+	insert := rethink.Map{"raw": rec.raw, "clean": rec.clean, "collectionID": collectionID}
+	err := rethink.Table("records").Insert(insert).Run(session).One(&response)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
--- disambiguate.go
+++ disambiguate.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	r "github.com/christopherhesse/rethinkgo"
+	"log"
 	"os"
-    "runtime/pprof"
-    "runtime"
-    "log"
-    "sync"
-    r "github.com/christopherhesse/rethinkgo"
+	"runtime"
+	"runtime/pprof"
+	"sync"
 )
 
 var filechannel = make(chan []byte)
@@ -16,53 +16,53 @@ var recordwg sync.WaitGroup
 
 func ingestRecords() {
 	for raw := range filechannel {
-        recordwg.Add(1)
-        recordChannel <- makeRecord(raw)
+		recordwg.Add(1)
+		recordChannel <- makeRecord(raw)
 	}
-    recordwg.Wait()
-    close(recordChannel)
+	recordwg.Wait()
+	close(recordChannel)
 }
 
 func pairwiseComparison() {
-    for i := 0; i < 100; i ++ {
-        pairwisewg.Add(1)
-        go func() {
-            for r := range recordChannel {
-                findMatch(r)
-                recordwg.Done()
-            }
-            pairwisewg.Done()
-        }()
-    }
-    pairwisewg.Wait()
+	for i := 0; i < 100; i++ {
+		pairwisewg.Add(1)
+		go func() {
+			for r := range recordChannel {
+				findMatch(r)
+				recordwg.Done()
+			}
+			pairwisewg.Done()
+		}()
+	}
+	pairwisewg.Wait()
 }
 
 func main() {
-    f, err := os.Create("disambiguator.cprof")
-    if err != nil {
-        log.Fatal(err)
-    }
-    pprof.StartCPUProfile(f)
-    defer pprof.StopCPUProfile()
+	f, err := os.Create("disambiguator.cprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 
-    runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-    session, err := r.Connect("localhost:28015", "disambiguate")
-    var response r.WriteResponse
-    r.Table("records").Delete().Run(session).One(&response)
+	session, err := r.Connect("localhost:28015", "disambiguate")
+	var response r.WriteResponse
+	r.Table("records").Delete().Run(session).One(&response)
 
 	filename := os.Args[1]
 	go readFile(filename, filechannel)
 	go ingestRecords()
 	pairwiseComparison()
-    runDisambiguations()
-    printOutput()
+	runDisambiguations()
+	printOutput()
 	filewg.Wait()
 
-    f, err = os.Create("disambiguator.mprof")
-    if err != nil {
-        log.Fatal(err)
-    }
-    pprof.WriteHeapProfile(f)
-    f.Close()
+	f, err = os.Create("disambiguator.mprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.WriteHeapProfile(f)
+	f.Close()
 }
